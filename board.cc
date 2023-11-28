0720868819b83480c8e87390e592cc16cc186f4a
s2suri <s2suri@uwaterloo.ca>
2023-11-28 14:58:47 -0500

    output operator

---
+++ board.cc
@@ -0,0 +1,27 @@
+#include "board.h";
+using namespace std;
+
+ostream& operator<<(ostream& out, const Board& board) {
+    int boardSize = 8;
+    vector<vector<char>> theDisplay;
+    for (int row = 0; row < boardSize; ++row) {
+        for (int col = 0; col < boardSize; ++col) {
+            if (board.getState()[row][col] != nullptr) {
+                theDisplay[row][col] = board.getState()[row][col]->displayChar(); 
+            } else {
+                // Set underscore for dark squares and space for light squares
+                theDisplay[row][col] = ((row + col) % 2 == 0) ? ' ' : '_';
+            }
+        }
+    }
+    
+    for (int row = 7; row >= 0; --row) {
+        out << row + 1 << ' '; // Print row number
+        for (int col = 0; col < 8; ++col) {
+            out << theDisplay[row][col] << ' ';
+        }
+        out << '\n';
+    }
+    out << " abcdefgh\n"; // Print column labels
+    return out;
+}

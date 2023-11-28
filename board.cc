3f67e0b578a18bfa4c6ae7277a1cf0189524b5af
Shivam Suri <shivam.suri@uwaterloo.ca>
2023-11-28 15:02:17 -0500

    Update board.cc

--- board.cc
+++ board.cc
@@ -1,6 +1,8 @@
 #include "board.h";
 using namespace std;
 
+Board::Board() : grid{8, vector<Piece*>(8,nullptr)}, turn{0}, isWin{false}, views{vector<View*>(2,nullptr)} {}
+
 ostream& operator<<(ostream& out, const Board& board) {
     int boardSize = 8;
     vector<vector<char>> theDisplay;

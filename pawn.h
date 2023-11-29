8ef855d3e58de911fa4b511a5948b29ecebec6d6
Ashwin Vigneswaramoorthy <a2vigneswaramoorthy@uwaterloo.ca>
2023-11-28 21:11:22 -0500

    Update pawn.h

--- pawn.h
+++ pawn.h
@@ -3,21 +3,18 @@
 #include "piece.h"
 
 class Pawn : public Piece {
+
+    bool moved;
+
 public:
-    Pawn(Type pieceType, string colour, Position pos, Board& board)
-        : Piece(pieceType, colour, pos, board) {}
+    Pawn(Type pieceType, string colour, Position pos, Board& board);
 
-    // Override the getPossibleMoves method for the Knight
-    //vector<Position> getPossibleMoves() const override;
+    // Override the getPossibleMoves method for the Pawn
+    vector<Position> getPossibleMoves() const override;
 
-    // Override the isValid method for the Knight
-    bool isValid(Position newPos) const override{
-        return true;
-    }
+    // Override the isValid method for the Pawn
 
-    char displayChar() const {
-        return 'P';
-    }
+    char displayChar() const;
 };
 
 #endif

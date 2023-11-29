28ba19d6cccfdd736289f606c67019dca9e340cb
s2suri <s2suri@uwaterloo.ca>
2023-11-28 19:55:18 -0500

    added queen impl

--- queen.h
+++ queen.h
@@ -4,20 +4,15 @@
 
 class Queen : public Piece {
 public:
-    Queen(Type pieceType, string colour, Position pos, Board& board)
-        : Piece(pieceType, colour, pos, board) {}
+    Queen(Type pieceType, string colour, Position pos, Board& board);
 
-    // Override the getPossibleMoves method for the Knight
-    //vector<Position> getPossibleMoves() const override;
+    // Override the getPossibleMoves method for the Queen
+    vector<Position> getPossibleMoves() const override;
 
-    // Override the isValid method for the Knight
-    bool isValid(Position newPos) const override{
-        return true;
-    }
+    // Override the isValid method for the Queen
+    bool isValid(Position newPos) const override;
 
-    char displayChar() const {
-        return 'Q';
-    }
+    char displayChar() const override;
 };
 
 #endif

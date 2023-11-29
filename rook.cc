8fbb6a1490d13ad9491688dc4fd0067a7ae2656c
Ashwin Vigneswaramoorthy <a2vigneswaramoorthy@uwaterloo.ca>
2023-11-28 21:11:53 -0500

    Update rook.cc

--- rook.cc
+++ rook.cc
@@ -1,1 +1,101 @@
+#include "rook.h"
+
+
+Rook::Rook(Type pieceType, string colour, Position pos, Board& board)
+        : Piece(pieceType, colour, pos, board) {}
+
+
+
+
+vector<Position> Rook::getPossibleMoves() const {
+
+    string oppColour;
+    vector<Position> temp; 
+
+    if(getColour() == "white") {
+        oppColour == "black";
+    } else if(getColour() == "black") {
+        oppColour == "white";
+    }
+    
+
+    // checking west side of rook
+    for(int i = pos.posX; i > 0; --i) {
+
+        if(theBoard.getState()[i][pos.posY] == nullptr) {
+            Position tempPos{i,pos.posY};
+            temp.push_back(tempPos);
+        } else if(theBoard.getState()[i][pos.posY] != nullptr && theBoard.getState()[i][pos.posY]->getColour() == oppColour) {
+            Position tempPos{i,pos.posY};
+            temp.push_back(tempPos);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    // checking east side of rook
+    for(int i = pos.posX; i < 8; ++i) {
+
+        if(theBoard.getState()[i][pos.posY] == nullptr) {
+            Position tempPos{i,pos.posY};
+            temp.push_back(tempPos);
+        } else if(theBoard.getState()[i][pos.posY] != nullptr && theBoard.getState()[i][pos.posY]->getColour() == oppColour) {
+            Position tempPos{i,pos.posY};
+            temp.push_back(tempPos);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    // checking north side of rook
+    for(int i = pos.posY; i > 0; --i) {
+
+        if(theBoard.getState()[pos.posX][i] == nullptr) {
+            Position tempPos{pos.posX,i};
+            temp.push_back(tempPos);
+        } else if(theBoard.getState()[pos.posX][i] != nullptr && theBoard.getState()[pos.posX][i]->getColour() == oppColour) {
+            Position tempPos{pos.posX,i};
+            temp.push_back(tempPos);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    // checking north side of rook
+    for(int i = pos.posY; i < 8; ++i) {
+
+        if(theBoard.getState()[pos.posX][i] == nullptr) {
+            Position tempPos{pos.posX,i};
+            temp.push_back(tempPos);
+        } else if(theBoard.getState()[pos.posX][i] != nullptr && theBoard.getState()[pos.posX][i]->getColour() == oppColour) {
+            Position tempPos{pos.posX,i};
+            temp.push_back(tempPos);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    return temp;
+}
+
+
+
+char Rook::displayChar() const {
+        
+    if(getColour() == "white") {
+        return 'r';
+    } else if(getColour() == "black") {
+        return 'R';
+    }
+    
+    
+}
+
+
+
+
 

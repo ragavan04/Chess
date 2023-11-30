2e15eb103cad523d675e57c65a3056012e8440b0
a2vignes <a2vignes@uwaterloo.ca>
2023-11-30 09:08:11 -0500

    empassant is working but gotta delete piece

--- pawn.cc
+++ pawn.cc
@@ -3,7 +3,7 @@
 
 
 Pawn::Pawn(Type pieceType, string colour, Position pos, Board& board)
-        : Piece(pieceType, colour, pos, board), moved{false} {}
+        : Piece(pieceType, colour, pos, board), moved{false}, enpassant{false}, oneTimeToggle{true} {}
 
 
 vector<Position> Pawn::getPossibleMoves() const {
@@ -28,6 +28,87 @@ vector<Position> Pawn::getPossibleMoves() const {
             Position tempPos{pos.posX + 1,pos.posY};
             temp.push_back(tempPos);      
         }
+
+        // checks for capture move on the left side
+        if(theBoard.getState()[pos.posX + 1][pos.posY - 1] != nullptr && theBoard.getState()[pos.posX + 1][pos.posY - 1]->getColour() != colour) { 
+            Position tempPos{pos.posX + 1,pos.posY - 1};
+            temp.push_back(tempPos);       
+        }
+
+        // checks for capture move on the right side 
+        if(theBoard.getState()[pos.posX + 1][pos.posY + 1] != nullptr && theBoard.getState()[pos.posX + 1][pos.posY + 1]->getColour() != colour) { 
+            Position tempPos{pos.posX + 1,pos.posY + 1};
+            temp.push_back(tempPos);       
+        }
+
+        // en passant move on left side
+        if(theBoard.getState()[pos.posX][pos.posY - 1] != nullptr && theBoard.getState()[pos.posX][pos.posY - 1]->getType() == Piece::PAWN
+            && theBoard.getState()[pos.posX][pos.posY - 1]->getColour() != colour) {
+            Pawn *pawn = dynamic_cast<Pawn*>(theBoard.getState()[pos.posX][pos.posY - 1]);
+            if(pawn->getEnpassant()) {
+                Position tempPos{pos.posX + 1,pos.posY - 1};
+                temp.push_back(tempPos);    
+            }
+        }
+
+        // en passant move on right side
+        if(theBoard.getState()[pos.posX][pos.posY + 1] != nullptr && theBoard.getState()[pos.posX][pos.posY + 1]->getType() == Piece::PAWN
+            && theBoard.getState()[pos.posX][pos.posY + 1]->getColour() != colour) {
+            Pawn *pawn = dynamic_cast<Pawn*>(theBoard.getState()[pos.posX][pos.posY + 1]);
+            if(pawn->getEnpassant()) {
+                Position tempPos{pos.posX + 1,pos.posY + 1};
+                temp.push_back(tempPos);    
+            }
+        }
+
+
+    }
+
+    if(getColour() == "black") { 
+        if(!moved && theBoard.getState()[pos.posX - 2][pos.posY] == nullptr) {
+            Position tempPos{pos.posX - 2,pos.posY};
+            temp.push_back(tempPos);        
+        }
+
+        if(theBoard.getState()[pos.posX - 1][pos.posY] == nullptr) {
+            Position tempPos{pos.posX - 1,pos.posY};
+            temp.push_back(tempPos);      
+        }
+
+        // checks for capture move on the left side
+        if(theBoard.getState()[pos.posX - 1][pos.posY - 1] != nullptr && theBoard.getState()[pos.posX + 1][pos.posY - 1]->getColour() != colour) { 
+            Position tempPos{pos.posX - 1,pos.posY - 1};
+            temp.push_back(tempPos);       
+        }
+
+        // checks for capture move on the right side 
+        if(theBoard.getState()[pos.posX - 1][pos.posY + 1] != nullptr && theBoard.getState()[pos.posX + 1][pos.posY + 1]->getColour() != colour) { 
+            Position tempPos{pos.posX - 1,pos.posY + 1};
+            temp.push_back(tempPos);       
+        }
+
+        // en passant move on left side
+        if(theBoard.getState()[pos.posX][pos.posY - 1] != nullptr && theBoard.getState()[pos.posX][pos.posY - 1]->getType() == Piece::PAWN
+            && theBoard.getState()[pos.posX][pos.posY - 1]->getColour() != colour) {
+            Pawn *pawn = dynamic_cast<Pawn*>(theBoard.getState()[pos.posX][pos.posY - 1]);
+            if(pawn->getEnpassant()) {
+                Position tempPos{pos.posX - 1,pos.posY - 1};
+                temp.push_back(tempPos);    
+            }
+        }
+
+        // en passant move on right side
+        if(theBoard.getState()[pos.posX][pos.posY + 1] != nullptr && theBoard.getState()[pos.posX][pos.posY + 1]->getType() == Piece::PAWN
+            && theBoard.getState()[pos.posX][pos.posY + 1]->getColour() != colour) {
+            Pawn *pawn = dynamic_cast<Pawn*>(theBoard.getState()[pos.posX][pos.posY + 1]);
+            if(pawn->getEnpassant()) {
+                Position tempPos{pos.posX - 1,pos.posY + 1};
+                temp.push_back(tempPos);    
+            }
+        }
+
+
+    }
  
     
 
@@ -45,7 +126,6 @@ vector<Position> Pawn::getPossibleMoves() const {
     //             temp.push_back(tempPos);
     //     }
     
-    } 
 
     // if(getColour() == "white") { 
     //     if(!moved && theBoard.getState()[pos.posY - 2][pos.posX] == nullptr) {

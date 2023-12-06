56015b4338425934a164f5a4e5a43118f78899c8
Ashwin Vigneswaramoorthy <a2vigneswaramoorthy@uwaterloo.ca>
2023-12-05 22:41:28 -0500

    Update levelthree.cc

--- levelthree.cc
+++ levelthree.cc
@@ -63,12 +63,12 @@ int LevelThree::evaluateMove(const pair<Position, Position> &move, Board* board)
        oppColour = "white"; 
     }
 
-    if (movingPiece != nullptr && movingPiece->getType() == Piece::KING && !board->isInCheckAfterMove(move.first, move.second, oppColour)) {
+    if (movingPiece != nullptr && movingPiece->getType() == Piece::KING && !board->isInCheckAfterMove(start, end, oppColour)) {
         return 1000;
     }
 
     // Score for checking the opponent's king
-    if (movingPiece != nullptr && movingPiece->getType() != Piece::KING && board->isInCheckAfterMove(move.first, move.second, oppColour)) {
+    if (movingPiece != nullptr && movingPiece->getType() != Piece::KING && board->isInCheckAfterMove(start, end, oppColour)) {
         checkScore += 20;
     }
 
